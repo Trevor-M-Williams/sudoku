921db8bca8cf80359777bcf26c4204c83e3aa7f0
Trevor Williams <tmw7991@gmail.com>
2024-11-19 16:44:51 -0700

    update difficulties

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,6 @@
 export const difficultyOptions = [
-  { label: "Easy", value: 0.2 },
+  { label: "Easy", value: 0.3 },
   { label: "Medium", value: 0.5 },
   { label: "Hard", value: 0.7 },
-  { label: "Expert", value: 0.8 },
-  { label: "Insane", value: 1 },
+  { label: "Expert", value: 999 },
 ];
